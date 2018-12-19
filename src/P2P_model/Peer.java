eead937192c9a5dcdd64302b3c3707ceeb888c1a
milad3797 <milad3797@gmail.com>
2018-12-19 18:52:07 +0330

    - p2p_model initialization

---
+++ src/P2P_model/Peer.java
@@ -0,0 +1,33 @@
+package P2P_model;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+
+public class Peer {
+    String ip;
+    ArrayList<Peer> peers;
+    DatagramSocket datagramSocket = new DatagramSocket(4490);
+
+    int generateRandomPort() {
+        return (int) (Math.random() * 62536 + 2000);
+    }
+
+    void request(String fileName) throws IOException {
+        DatagramSocket udpSocket = new DatagramSocket();
+        byte[] reqBuffer = fileName.getBytes();
+        for (Peer anPeer : peers) {
+            DatagramPacket reqPacket = new DatagramPacket(reqBuffer, reqBuffer.length, InetAddress.getByName(anPeer.ip), generateRandomPort());
+            udpSocket.send(reqPacket);
+        }
+        DatagramPacket reqPacket = new DatagramPacket(fileName.getBytes(), fileName.getBytes().length);
+        udpSocket.send(new DatagramPacket());
+
+
+    }
+
+    void listen() {
+
+    }
+
+}
