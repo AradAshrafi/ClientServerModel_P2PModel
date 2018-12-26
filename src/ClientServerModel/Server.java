7e5f7ed843b579387de8cdc72b53994712fdd883
Arad.Ashrafi@gmail.com <Arad.Ashrafi@gmail.com>
2018-12-26 21:50:31 +0330

    Final Revision and Fix Git Problems

--- src/ClientServerModel/Server.java
+++ src/ClientServerModel/Server.java
@@ -3,8 +3,6 @@ package ClientServerModel;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Server {
     private static int PORT = 7654;
@@ -18,7 +16,8 @@ public class Server {
                 Socket client = server.accept();
                 count++;
                 System.out.println("client accepted!");
-                new ClientHandler(client).run();
+                ClientHandler clientHandler = new ClientHandler(client);
+                clientHandler.run();
             }
             System.out.print("done.\nClosing server ... ");
         } catch (IOException ex) {
@@ -26,32 +25,6 @@ public class Server {
         }
         System.out.println("done.");
     }
-
-    private static void parse_commands(String command) {
-        String[] parsedCommand = command.split(" ");
-        switch (parsedCommand[0]) {
-            case ("Add"):
-
-            case ("Subtract"):
-
-            case ("Divide"):
-
-            case ("Multiply"):
-
-            case ("Sin"):
-
-            case ("Cos"):
-
-            case ("Tan"):
-
-            case ("Cot"):
-
-
-            default:
-                System.out.println("Wrong Syntax");
-        }
-    }
-
 }
 
 
@@ -67,9 +40,12 @@ class ClientHandler implements Runnable {
     public void run() {
         try {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            PrintWriter printWriter = new PrintWriter(client.getOutputStream());
+            PrintWriter printWriter = new PrintWriter(client.getOutputStream(), true);
             String command = bufferedReader.readLine();
-            System.out.println(command);
+            String result = parse_commands(command);
+
+            printWriter.println(result);
+            printWriter.flush();
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -80,5 +56,50 @@ class ClientHandler implements Runnable {
                 System.err.println(ex);
             }
         }
+    }
+
+
+    private String parse_commands(String command) {
+        String[] parsedCommand = command.split(" ");
+        int op1 = Integer.parseInt(parsedCommand[1]);
+        int op2;
+        String result;
+        long startTime = System.nanoTime();
+        switch (parsedCommand[0]) {
+            case ("Add"):
+                op2 = Integer.parseInt(parsedCommand[2]);
+                result = Integer.toString((op1 + op2));
+                break;
+            case ("Subtract"):
+                op2 = Integer.parseInt(parsedCommand[2]);
+                result = Integer.toString((op1 - op2));
+                break;
+            case ("Divide"):
+                op2 = Integer.parseInt(parsedCommand[2]);
+                result = Double.toString((op1 * 1.0 / op2));
+                break;
+            case ("Multiply"):
+                op2 = Integer.parseInt(parsedCommand[2]);
+                result = Integer.toString((op1 * op2));
+                break;
+            case ("Sin"):
+                result = Double.toString(Math.sin(op1));
+                break;
+            case ("Cos"):
+                result = Double.toString(Math.cos(op1));
+                break;
+            case ("Tan"):
+                result = Double.toString(Math.tan(op1));
+                break;
+            case ("Cot"):
+                result = Double.toString(Math.tan(Math.PI / 2 - op1));
+                break;
+            default:
+                result = "Wrong Syntax";
+        }
+        long endTime = System.nanoTime();
+        long totalTIme = endTime - startTime;
+        return "Execution time in milliseconds : " +
+                totalTIme * 1.0 / 1000000 + " " + "Result = " + result;
     }
 }
